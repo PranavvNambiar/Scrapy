6c58b4d3e2a12cdaa5a02986a9607e6143157e21
Pranav <pranav04092002@gmail.com>
2024-05-11 01:59:23 +0530

    Second Commit

--- bookscraper/pipelines.py
+++ bookscraper/pipelines.py
@@ -10,4 +10,58 @@ from itemadapter import ItemAdapter
 
 class BookscraperPipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        
+        #*If we get an error, 'NoneType' object is not subscriptable
+        #*It means that a field name cannot be found in the main spider.
+        #*In this case, cross reference the items and the items defined in the main spider.
+        
+        #Used for striping the whitespaces from the strings
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != 'description':
+                value = adapter.get(field_name)
+                adapter[field_name] = value[0].strip()
+        
+        #Category & Product Type => Switch to lowercase
+        lowercase_keys = ['category','product_type']
+        for lowercase_key in lowercase_keys:
+                value = adapter.get(lowercase_key)
+                adapter[lowercase_key] = value.lower()
+        
+        price_keys = ['price','price_excl_tax','price_incl_tax']
+        for price_key in price_keys:
+                value = adapter.get(price_key)
+                value = value.replace('£','')
+                adapter[price_key] = float(value)
+                
+        ## Availability --> extract number of books in stock
+        availability_string = adapter.get('availability')
+        split_string_array = availability_string.split('(')
+        if len(split_string_array) < 2:
+            adapter['availability'] = 0
+        else:
+            availability_array = split_string_array[1].split(' ')
+            adapter['availability'] = int(availability_array[0])
+            
+        ## Reviews --> convert string to number
+        num_reviews_string = adapter.get('num_reviews')
+        adapter['num_reviews'] = int(num_reviews_string)
+        
+        ## Stars --> convert text to number
+        stars_string = adapter.get('stars')
+        split_stars_array = stars_string.split(' ')
+        stars_text_value = split_stars_array[1].lower()
+        if stars_text_value == "zero":
+            adapter['stars'] = 0
+        elif stars_text_value == "one":
+            adapter['stars'] = 1
+        elif stars_text_value == "two":
+            adapter['stars'] = 2
+        elif stars_text_value == "three":
+            adapter['stars'] = 3
+        elif stars_text_value == "four":
+            adapter['stars'] = 4
+        elif stars_text_value == "five":
+            adapter['stars'] = 5
         return item
